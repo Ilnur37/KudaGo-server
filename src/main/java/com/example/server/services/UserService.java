bb7a36370345807ff54ce6ec40b91618c0114bac
Ilnur37 <dinmukhameto@mail.ru>
2023-04-05 22:59:58 +0300

    com

---
+++ src/main/java/com/example/server/services/UserService.java
@@ -0,0 +1,46 @@
+package com.example.server.services;
+
+import com.example.server.entity.User;
+import com.example.server.entity.enums.ERole;
+import com.example.server.exceptions.UserExistException;
+import com.example.server.payload.request.SignupRequest;
+import com.example.server.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(ERole.ROLE_USER);
+
+        try {
+            LOG.info("Saving User {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception e) {
+            LOG.info("Error during registration, {}", e.getMessage());
+            throw new UserExistException("The user " + user.getUsername() + "already exist");
+        }
+    }
+
+
+}
